ad1511a2863c85f71cd0b3aca2d8c7459a1ed1f4
Andrey Kachur <underfil@mail.ru>
2022-04-11 10:22:51 +0300

    Clean init project

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './assets/styles/index.scss';
+import App from './components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
